0ef2b7fe7ea1215a347e84f323105fba146942ed
Paregos <ben.mitchell58@gmail.com>
2016-08-06 16:10:54 +1200

    Signifcantly refactored the Input.java class into clear methods and modules, at the current commit all functionality is working

--- InputParser/src/inputParser/Input.java
+++ InputParser/src/inputParser/Input.java
@@ -10,31 +10,85 @@ import org.graphstream.graph.implementations.DefaultGraph;
 import org.graphstream.stream.file.FileSource;
 import org.graphstream.stream.file.FileSourceFactory;
 
-
-
-
-
 /**
  * Created by Ben on 8/4/2016.
  */
 public class Input {
 
-	public static ArrayList<ArrayList<Integer>> adjacencyList = new ArrayList<ArrayList<Integer>>();
-	public static int[] bottomLevels;
-	public static Graph g;
+	private Graph inputG;
+	private ArrayList<ArrayList<Integer>> adjList = new ArrayList<ArrayList<Integer>>();
+	private int[] botLevels;
+	private int adjMatrix[][];
+	private ArrayList<Integer> srcNodes = new ArrayList<Integer>();
 	
-    public static void main(String[] args) throws IOException {
-
+	public ArrayList<Integer> getSrcNodes() {
+		return srcNodes;
+	}
+	public void setSrcNodes(ArrayList<Integer> srcNodes) {
+		this.srcNodes = srcNodes;
+	}
+	public int[][] getAdjMatrix() {
+		return adjMatrix;
+	}
+	public void setAdjMatrix(int[][] adjMatrix) {
+		this.adjMatrix = adjMatrix;
+	}
+	public ArrayList<ArrayList<Integer>> getAdjList() {
+		return adjList;
+	}
+	public void setAdjList(ArrayList<ArrayList<Integer>> adjList) {
+		this.adjList = adjList;
+	}
+	public int[] getBotLevels() {
+		return botLevels;
+	}
+	public void setBotLevels(int[] botLevels) {
+		this.botLevels = botLevels;
+	}
+    public Graph getInputG() {
+		return inputG;
+	}
+	public void setInputG(Graph inputG) {
+		this.inputG = inputG;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Input input = new Input();
+		
     	//pass in the input file name
-        Graph g = parseInput("src/test.dot");       
+        input.readInputGraph("src/test.dot");
+        
+        //creating the adjacency matrix and list
+        input.createMatrixList();
+        
+        //finding all of the source nodes
+        input.createSourceNodes();
+        
+        //creating a phantomSource node
+        ArrayList phantomSource = input.createPhantomSource();
+        
+        //instantiating the bottom level array
+        input.setBotLevels(new int[input.getInputG().getNodeCount()]);
+        
+        //using phantomSource to find all bottom levels and critical path
+        int criticalPath = input.createBottomLevels(phantomSource);
+        
+        //removing the phantom node
+        input.deletePhatomSource();
+        
+        //printing critical path and bottom levels
+        System.out.println("The critical path of this input graph is " + criticalPath);
+        System.out.println("------------------------------");
+        for (int j = 0; j < input.getInputG().getNodeCount(); j++){
+            System.out.print("The bottom level for node index[" + j +"] is " +  input.getBotLevels()[j] +"\n");
+        }
         
     }
 
-
-    public static Graph parseInput(String fileName) throws IOException {
-
-        //create the graph object
-        Graph inputGraph= new DefaultGraph("inputGraph");
+	private void readInputGraph(String fileName) throws IOException {
+		
+		//create the graph object
+        Graph inputGraph = new DefaultGraph("inputGraph");
         FileSource fs = FileSourceFactory.sourceFor(fileName);
         fs.addSink(inputGraph);
 
@@ -56,16 +110,14 @@ public class Input {
             fs.removeSink(inputGraph);
         }
 
-
-        //displaying graph --- can delete this ---
-		for (Node node : inputGraph) {
-		        node.addAttribute("ui.label", node.getId());
-		        node.setAttribute("size", "small");
-		        System.out.println(node.getAttribute("weight"));
-		    }
-        inputGraph.display();
-        
-        //creating the adjacencyMatrix of the input file graph
+        this.setInputG(inputGraph);
+	}
+	
+    private void createMatrixList (){
+    	
+    	Graph inputGraph = this.getInputG();
+    	
+    	//creating the adjacencyMatrix of the input file graph
         int numNodes = inputGraph.getNodeCount();
         int adjacencyMatrix[][] = new int[numNodes][numNodes];
         for (int i = 0; i < numNodes; i++){
@@ -73,132 +125,110 @@ public class Input {
                 adjacencyMatrix[i][j] = (inputGraph.getNode(i).hasEdgeBetween(j) ? Integer.parseInt(inputGraph.getNode(i).getEdgeBetween(j).getAttribute("weight")) : -1);
             }
         }
-
-        //printing the adjacency Matrix --- can delete this ---
-        for (int i = 0; i < numNodes; i++){
-            for (int j = 0; j < numNodes; j++){
-                System.out.print(adjacencyMatrix[i][j] + ",");
-            }
-            System.out.println("");
-        }
-
+        this.setAdjMatrix(adjacencyMatrix);
         
-        // Adjacency list
+   
+        // creating the Adjacency list of the input file graph
+        ArrayList temp1 = new ArrayList();
         for (int i = 0; i < numNodes; i++) {
         	ArrayList temp = new ArrayList();
-        	adjacencyList.add(temp);
+        	temp1.add(temp);
         	for (int j = 0; j < numNodes; j++) {
         		if (adjacencyMatrix[i][j] != -1){
         			temp.add(j);
         		}
         	}
         }
+        this.setAdjList(temp1);
         
-        
-        //printing oout the adjaceny list to check if its correct --- can delete this ---
-        System.out.println("------------------------------");
-        for (int i = 0; i < adjacencyList.size(); i++){
-            for (int j = 0; j < adjacencyList.get(i).size(); j++){
-                System.out.print(adjacencyList.get(i).get(j) + ",");
-            }
-            System.out.println("");
-        }
-        System.out.println("------------------------------");
-        
-        
+    }
+    
+    private void createSourceNodes (){
+    	
+    	Graph inputGraph = this.getInputG();
+        int numNodes = inputGraph.getNodeCount();
+    	
         // Finds all source nodes
         ArrayList<Integer> sourceNodes = new ArrayList<Integer>();
         int counter = 0;       
         for (int k = 0; k < numNodes; k++) {
         	counter = 0;
-        	for (int i = 0; i < adjacencyList.size(); i++) {
-        		for (int j = 0; j < adjacencyList.get(i).size(); j++) {
-
-        			if ((adjacencyList.get(i).get(j).equals(k) && ( k!= i))) {
+        	for (int i = 0; i < this.getAdjList().size(); i++) {
+        		for (int j = 0; j < this.getAdjList().get(i).size(); j++) {
+        			if ((this.getAdjList().get(i).get(j).equals(k) && ( k!= i))) {
         				counter++;
         			} 
-        			
         		}
         	}
         	if (counter == 0){
         		sourceNodes.add(k);
         	}
     	}
-        System.out.println(sourceNodes);
-        System.out.println(sourceNodes.size());
+      
+        this.setSrcNodes(sourceNodes);
         
+    }
+    
+    private ArrayList createPhantomSource (){
         
-        //=============put in method===========
+    	Graph inputGraph = this.getInputG();
+    	ArrayList<Integer> sourceNodes = this.getSrcNodes();
+    	
         //constructing a phantom node to be single source
         inputGraph.addNode("source1");
         inputGraph.getNode("source1").addAttribute("weight", "0");
         //adding it to the adjacency list with edges to real sources
+        ArrayList<ArrayList<Integer>> adjacencyList = this.getAdjList();
         adjacencyList.add(sourceNodes);
+        
+        //setting the values of the newly changed list and graph
+        this.setInputG(inputGraph);
+        this.setAdjList(adjacencyList);
+        
+        //adding the new phantom node to a arraylist of its own which will be used to find all bottom levels
         ArrayList singleSource = new ArrayList();
-        singleSource.add(numNodes);
+        singleSource.add(inputGraph.getNodeCount() -1);
         
-        
-        //need to assign value to g so that bottom level function can access values
-        g = inputGraph;
-        
-        //bottom level generation
-        bottomLevels = new int[numNodes + 1];
-        getbottom(singleSource);
-
-        
-        //printing out the bottom levels
-        System.out.println("------------------------------");
-        for (int j = 0; j < numNodes; j++){
-            System.out.print(" the bottom level for " + j +" is " +  bottomLevels[j] +"\n");
-        }
-        
-         
-        return inputGraph;
+        return singleSource;
     }
     
-    public static int getbottom(ArrayList<Integer> nodes){
-		
-    	for (Integer i: nodes){
-    		System.out.println("-------------START OF NEW CALL-----------------");
-    		System.out.println("Im looking at node "+ i +" the value of i is " + i);
-    		ArrayList<Integer> children = adjacencyList.get(i);
-    		
-    		System.out.println(children.isEmpty());
-    		
-    		for(Integer j : children){
-				System.out.println("node" + i + " children are " + j);
-			}
-    		
-    		
-    		if(!children.isEmpty()){
-    			System.out.println("node" + i + " has " + children.size()+" number of children"  +" the value of i is " + i);
-    			ArrayList<Integer> cBottomLevels = new ArrayList<Integer>();
-    				for(Integer j : children){
-    					System.out.println("recusively looking at node " + j  +" the value of j is " + j);
-    					ArrayList<Integer> singleChild = new ArrayList<Integer>();
-    					singleChild.add(j);
-    					System.out.println("recusively looking at node " + j +" the value of j is " + j);
-    					cBottomLevels.add(getbottom(singleChild));
-    				}
-    				System.out.println("Im adding a value to bottomlevels for node "+i);
-    				bottomLevels[i] = ( Integer.parseInt(g.getNode(i).getAttribute("weight")) + Collections.max(cBottomLevels));
-    				return (Integer.parseInt(g.getNode(i).getAttribute("weight")) + Collections.max(cBottomLevels));
-    		} else {
-    			//System.out.println(g.getNode(i).getAttribute("weight"));
-    			System.out.println("Im adding a value to bottomlevels for node "+i);
-    			bottomLevels[i] = Integer.parseInt(g.getNode(i).getAttribute("weight"));
-//    			System.out.println("Im looking at node "+ i +" the value of bottomLevels[i] is " + bottomLevels[i]);
-    			
-    			return Integer.parseInt(g.getNode(i).getAttribute("weight"));
-    		}
-    		
-    		
-    		
-    		
-    	}
-		return 0;
+    private void deletePhatomSource (){
+    	
+    	Graph inputGraph = this.getInputG();
+    	inputGraph.removeNode("source1");
+    	
+    	ArrayList<ArrayList<Integer>> adjacencyList = this.getAdjList();
+    	adjacencyList.remove(inputGraph.getNodeCount());
     	
     }
+    
+    private int createBottomLevels(ArrayList<Integer> nodes){
+		
+    	for (Integer i: nodes){
+    		ArrayList<Integer> children = this.getAdjList().get(i);
+	
+    		if(!children.isEmpty()){
+    			ArrayList<Integer> cBottomLevels = new ArrayList<Integer>();
+    			
+    				for(Integer j : children){
+    					ArrayList<Integer> singleChild = new ArrayList<Integer>();
+    					singleChild.add(j);
+    					cBottomLevels.add(this.createBottomLevels(singleChild));
+    				}
+    				int[] temp = this.getBotLevels();
+    				temp[i] = ( Integer.parseInt(this.getInputG().getNode(i).getAttribute("weight")) + Collections.max(cBottomLevels));
+    				this.setBotLevels(temp);
+    				return (Integer.parseInt(this.getInputG().getNode(i).getAttribute("weight")) + Collections.max(cBottomLevels));
+    		} else {
+    			int[] temp = this.getBotLevels();
+    			temp[i] = Integer.parseInt(this.getInputG().getNode(i).getAttribute("weight"));
+    			this.setBotLevels(temp);
+    			
+    			return Integer.parseInt(this.getInputG().getNode(i).getAttribute("weight"));
+    		}
+    	}
+		return 0;
+    }
 
-
+  
 }
