2ecf410ffeabab1cc12b3d24aefa1329a0c5729a
Ammar Bagasrawala <abag505@EN-284083.UoA.auckland.ac.nz>
2016-08-07 19:06:34 +1200

    Fixed layout to include both graph and process panels

--- src/gui/VFrame.java
+++ src/gui/VFrame.java
@@ -3,7 +3,9 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -39,16 +41,26 @@ public class VFrame {
 		mainFrame.setSize(1000,800);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
 
-		contentPane = new JPanel();
-		contentPane.setLayout(new BorderLayout());
-		contentPane.setPreferredSize(new Dimension(500, 500));
+		JPanel contentPane = new JPanel();
+		contentPane.setLayout(new GridLayout(1, 2));
+		
+		JPanel graphPane = new JPanel();
+		graphPane.setLayout(new BorderLayout());
+		graphPane.setPreferredSize(new Dimension(500, 800));
 		
 		Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
 		View view = viewer.addDefaultView(false);
-		((Component) view).setPreferredSize(new Dimension(500, 500));
-		contentPane.add((Component) view, BorderLayout.CENTER);
 		viewer.enableAutoLayout();
+		//((Component) view).setPreferredSize(new Dimension(500, 500));
+		graphPane.add((Component) view, BorderLayout.CENTER);
 		
+		JPanel processPanel = new JPanel();
+		processPanel.setLayout(new BorderLayout());
+		JButton button = new JButton("hello");
+		//processPanel.add(button);
+		
+		contentPane.add(graphPane);
+		contentPane.add(processPanel);
 		mainFrame.add(contentPane);
 		//mainFrame.pack();
 	}
