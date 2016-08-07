139a3653bed976992dd4fb3aec396ca0e185b5e7
Hanzhi Wang <hanzhi2@gmail.com>
2016-08-07 18:39:03 +1200

    added main method and algorithm for generating schedule

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -7,10 +9,30 @@ public class Graph {
     private int[] nodeCosts;
     private int[] nodeBL;
     private int[][] edgeCosts;
-    private List<Integer>[] adjListChildren;
-    private List<Integer>[] adjListDependencies;
+    private List<List<Integer>> adjListChildren;
+    private List<List<Integer>> adjListDependencies;
+    private int numProcessors =2;
+    private int totalTaskTime = 10;
+    private int totalNumTasks = 4;
 
     private static Graph instance;
+
+    private Graph(){
+        nodeCosts=new int[]{2,3,3,2};
+        nodeBL=new int[]{7,5,5,2};
+        edgeCosts=new int[][]{{-1,1,2,-1},{-1,-1,-1,2},{-1,-1,-1,1},{-1,-1,-1,-1}};
+        adjListChildren=new ArrayList<>();
+        adjListChildren.add(Arrays.asList(new Integer[]{1,2}));
+        adjListChildren.add(Arrays.asList(new Integer[]{3}));
+        adjListChildren.add(Arrays.asList(new Integer[]{3}));
+        adjListChildren.add(Arrays.asList(new Integer[]{}));
+
+        adjListDependencies=new ArrayList<>();
+        adjListDependencies.add(Arrays.asList(new Integer[]{}));
+        adjListDependencies.add(Arrays.asList(new Integer[]{0}));
+        adjListDependencies.add(Arrays.asList(new Integer[]{0}));
+        adjListDependencies.add(Arrays.asList(new Integer[]{1,2}));
+    }
 
     public static Graph getInstance(){
         if(instance==null) {
@@ -20,24 +42,39 @@ public class Graph {
     }
 
     int getNodeCost(int node){
-        return 0;
+        return nodeCosts[node];
     }
 
     int getBottomLevel(int node){
-        return 0;
+        return nodeBL[node];
     }
 
     int getEdgeCost(int src, int dest){
-        return 0;
+        return edgeCosts[src][dest];
     }
 
     List<Integer> getChildren(int node){
-        return null;
+        return adjListChildren.get(node);
     }
 
     List<Integer> getDependencies(int node){
-        return null;
+        return adjListDependencies.get(node);
+    }
+
+    public int getNumProcessors() {
+        return numProcessors;
     }
 
 
+    public int getTotalTaskTime() {
+        return totalTaskTime;
+    }
+
+    public int getTotalNumTasks() {
+        return totalNumTasks;
+    }
+
+    public List<Integer> getEntryPoints(){
+        return Arrays.asList(new Integer[]{0});
+    }
 }
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Hanzhi on 5/08/2016.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println(new Algorithm().schedule(Graph.getInstance()));
+    }
+}
