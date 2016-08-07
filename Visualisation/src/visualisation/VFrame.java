b29c5fc903560781de8bc163c121cd2b1bc063a9
Ammar Bagasrawala <abag505@EN-284083.UoA.auckland.ac.nz>
2016-08-07 14:20:13 +1200

    Created project, package and class for visualisation

---
+++ Visualisation/src/visualisation/VFrame.java
@@ -0,0 +1,18 @@
+package visualisation;
+
+
+/**
+ * Created on 7/08/16
+ * @author Ammar Bagasrawala
+ *
+ */
+public class VFrame {
+	
+	public static void main(String args[]) {
+		
+		
+		
+		
+	}
+	
+}
