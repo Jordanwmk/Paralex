e243b16e1a3f4efd82e20938493263208c95debc
Jordanwmk <jwon223@aucklanduni.ac.nz>
2016-08-07 18:44:55 +1200

    tried autolayout gStream

--- src/gui/VFrame.java
+++ src/gui/VFrame.java
@@ -1,5 +1,17 @@
 package gui;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.DefaultGraph;
+import org.graphstream.graph.implementations.MultiGraph;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.view.View;
+import org.graphstream.ui.view.Viewer;
+
 
 /**
  * 
@@ -7,10 +19,46 @@ package gui;
  * @author Ammar Bagasrawala
  *
  */
-public class VFrame {
+public class VFrame{
 
-	public static void main (String[] args) {
-		
-	}
+	private static Graph graph = new SingleGraph("Tutorial 1");
+
+    public static class MyFrame extends JFrame
+    {
+        private static final long serialVersionUID = 8394236698316485656L;
+
+        //private Graph graph = new MultiGraph("embedded");
+        private Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
+        //private Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_SWING_THREAD);
+        private View view = viewer.addDefaultView(false);
+
+        public MyFrame() {
+             setLayout(new BorderLayout());
+             this.add(BorderLayout.CENTER);
+             setDefaultCloseOperation(EXIT_ON_CLOSE);
+             viewer.enableAutoLayout();
+        }
+    }
+
+    public static void main(String args[]) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                MyFrame frame = new MyFrame();
+                frame.setSize(320, 240);
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+
+                graph.addNode("A");
+                graph.addNode("B");
+                graph.addNode("C");
+                graph.addEdge("AB", "A", "B");
+                graph.addEdge("BC", "B", "C");
+                graph.addEdge("CA", "C", "A");
+
+                graph.addAttribute("ui.quality");
+                graph.addAttribute("ui.antialias");
+            }
+        });
+    }
 	
 }
