113b829e0c3b0e7ecc3fdbbc43ec18c51c0bb5d7
plokji <okplokjiloop@hotmail.com>
2016-08-08 14:06:20 +1200

    Added timings for the brute force and A Star algorithms

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,22 @@
  */
 public class Main {
     public static void main(String[] args){
+
+        long startTime = System.currentTimeMillis();
         Schedule bruteForceSolution=new BruteForceAlgorithm().schedule(Graph.getInstance());
         System.out.println(bruteForceSolution);
         System.out.println("brute force total time: " + bruteForceSolution.getTotalTime());
         System.out.println();
+        long endTime   = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("Brute Force Time: " + totalTime);
 
+        startTime = System.currentTimeMillis();
         Schedule aStarSolution=new AStarAlgorithm().schedule(Graph.getInstance());
         System.out.println(aStarSolution);
         System.out.println("a* total time: "+aStarSolution.getTotalTime());
+        endTime   = System.currentTimeMillis();
+        totalTime = endTime - startTime;
+        System.out.println("A Star Time: " + totalTime);
     }
 }
