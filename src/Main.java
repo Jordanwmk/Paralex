e662b2a99ea73c82dd821b2bd6ec3eb956e13e90
Hanzhi Wang <hanzhi2@gmail.com>
2016-08-08 13:33:58 +1200

    added brute force algorithm implementation

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println(new Algorithm().schedule(Graph.getInstance()));
+        Schedule bruteForceSolution=new BruteForceAlgorithm().schedule(Graph.getInstance());
+        System.out.println(bruteForceSolution);
+        System.out.println("brute force total time: " + bruteForceSolution.getTotalTime());
+        System.out.println();
+
+        Schedule aStarSolution=new AStarAlgorithm().schedule(Graph.getInstance());
+        System.out.println(aStarSolution);
+        System.out.println("a* total time: "+aStarSolution.getTotalTime());
     }
 }
