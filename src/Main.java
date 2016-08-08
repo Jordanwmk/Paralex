beaed1bb33855161559bb0ff7f698ac63161e234
plokji <okplokjiloop@hotmail.com>
2016-08-08 14:10:02 +1200

    Reordered the timer

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,21 @@ public class Main {
 
         long startTime = System.currentTimeMillis();
         Schedule bruteForceSolution=new BruteForceAlgorithm().schedule(Graph.getInstance());
-        System.out.println(bruteForceSolution);
-        System.out.println("brute force total time: " + bruteForceSolution.getTotalTime());
-        System.out.println();
         long endTime   = System.currentTimeMillis();
         long totalTime = endTime - startTime;
         System.out.println("Brute Force Time: " + totalTime);
+        System.out.println(bruteForceSolution);
+        System.out.println("brute force total time: " + bruteForceSolution.getTotalTime());
+        System.out.println();
 
         startTime = System.currentTimeMillis();
         Schedule aStarSolution=new AStarAlgorithm().schedule(Graph.getInstance());
-        System.out.println(aStarSolution);
-        System.out.println("a* total time: "+aStarSolution.getTotalTime());
         endTime   = System.currentTimeMillis();
         totalTime = endTime - startTime;
         System.out.println("A Star Time: " + totalTime);
+
+        System.out.println(aStarSolution);
+        System.out.println("a* total time: "+aStarSolution.getTotalTime());
+
     }
 }
