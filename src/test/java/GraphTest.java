27837b7681cc7d3f1d93177f2b71892e4612bbe4
Paregos <ben.mitchell58@gmail.com>
2016-08-25 10:16:22 +1200

    Fixed test cases and removed random files

--- src/test/java/GraphTest.java
+++ src/test/java/GraphTest.java
@@ -34,121 +34,127 @@ public class GraphTest {
 		}
 	};
 
+	//===================================================================================
+	//===================================================================================
+	//====================				  2 Processors 		     ========================
+	//==================================Series Non Visual================================
+	//===================================================================================
+	
 	@Test
-	public void test_Nodes_11_2_Proccessors() throws IOException {
+	public void test_Nodes_11_2_Proccessors_Serial() throws IOException {
 		Schedule solution = new BranchAndBoundAlgorithm().schedule(new TaskGraph("src/test/resources/Nodes_11_OutTree.dot", 2));
 		assertEquals(350, solution.getTotalTime());
 
 	} 
 	
 	@Test
-	public void test_Nodes_10_2_Proccessors() throws IOException {
+	public void test_Nodes_10_2_Proccessors_Serial() throws IOException {
 		Schedule solution = new BranchAndBoundAlgorithm().schedule(new TaskGraph("src/test/resources/Nodes_10_Random.dot", 2));
 		assertEquals(50, solution.getTotalTime());
 	} 
 	
 	@Test
-	public void test_Nodes_9_2_Proccessors() throws IOException {
+	public void test_Nodes_9_2_Proccessors_Serial() throws IOException {
 		Schedule solution = new BranchAndBoundAlgorithm().schedule(new TaskGraph("src/test/resources/Nodes_9_SeriesParallel.dot", 2));
 		assertEquals(55, solution.getTotalTime());
 	} 
 	
 	@Test
-	public void test_Nodes_8_2_Proccessors() throws IOException {
+	public void test_Nodes_8_2_Proccessors_Serial() throws IOException {
 		Schedule solution = new BranchAndBoundAlgorithm().schedule(new TaskGraph("src/test/resources/Nodes_8_Random.dot", 2));
 		assertEquals(581, solution.getTotalTime());
 
 	} 
 	
 	@Test
-	public void test_Nodes_7_2_Proccessors() throws IOException {
+	public void test_Nodes_7_2_Proccessors_Serial() throws IOException {
 		Schedule solution = new BranchAndBoundAlgorithm().schedule(new TaskGraph("src/test/resources/Nodes_7_OutTree.dot", 2));
 		assertEquals(28, solution.getTotalTime());
 	} 
 	
 	@Test
-	public void test_9middle_ForkandJoin_2_Proccessors() throws IOException {
-		Schedule solution = new BranchAndBoundAlgorithm().schedule(new TaskGraph("src/test/resources/forkjoin_9middle.dot", 2));
+	public void test_forkjoin9_2_Proccessors_Serial() throws IOException {
+		Schedule solution = new BranchAndBoundAlgorithm().schedule(new TaskGraph("src/test/resources/forkjoin9.dot", 2));
 		assertEquals(23, solution.getTotalTime());
 	} 
 	
 	//===================================================================================
 	//===================================================================================
 	//====================				  4 Processors 		     ========================
-	//===================================================================================
+	//==================================Series Non Visual================================
 	//===================================================================================
 	
 	@Test
-	public void test_Nodes_10_4_Proccessors() throws IOException {
-		Schedule solution = new BranchAndBoundAlgorithm().schedule(new TaskGraph("src/test/resources/Nodes_10_Random.dot", 4));
-		assertEquals(50, solution.getTotalTime());
-	} 
-	
-	@Test
-	public void test_Nodes_11_4_Proccessors() throws IOException {
+	public void test_Nodes_11_4_Proccessors_Serial() throws IOException {
 		Schedule solution = new BranchAndBoundAlgorithm().schedule(new TaskGraph("src/test/resources/Nodes_11_OutTree.dot", 4));
 		assertEquals(227, solution.getTotalTime());
 	}
 	
 	@Test
-	public void test_Nodes_9_4_Proccessors() throws IOException {
+	public void test_Nodes_10_4_Proccessors_Serial() throws IOException {
+		Schedule solution = new BranchAndBoundAlgorithm().schedule(new TaskGraph("src/test/resources/Nodes_10_Random.dot", 4));
+		assertEquals(50, solution.getTotalTime());
+	} 
+	
+	@Test
+	public void test_Nodes_9_4_Proccessors_Serial() throws IOException {
 		Schedule solution = new BranchAndBoundAlgorithm().schedule(new TaskGraph("src/test/resources/Nodes_9_SeriesParallel.dot", 4));
 		assertEquals(55, solution.getTotalTime());
 	} 
 	
 	@Test
-	public void test_Nodes_8_4_Proccessors() throws IOException {
+	public void test_Nodes_8_4_Proccessors_Serial() throws IOException {
 		Schedule solution = new BranchAndBoundAlgorithm().schedule(new TaskGraph("src/test/resources/Nodes_8_Random.dot", 4));
 		assertEquals(581, solution.getTotalTime());
 	}
 	
 	@Test
-	public void test_Nodes_7_4_Proccessors() throws IOException {
+	public void test_Nodes_7_4_Proccessors_Serial() throws IOException {
 		Schedule solution = new BranchAndBoundAlgorithm().schedule(new TaskGraph("src/test/resources/Nodes_7_OutTree.dot", 4));
 		assertEquals(22, solution.getTotalTime());
 	} 
-
+	
 	//===================================================================================
 	//===================================================================================
-	//====================				  Parallel 2 Cores 		     ========================
-	//===================================================================================
+	//====================				  2 Processors 		     ========================
+	//=================================2 Parallel Non Visual=============================
 	//===================================================================================
 	
 	
 	@Test
-	public void test_Nodes_11_2_Proccessors_Parallel() throws IOException {
+	public void test_Nodes_11_2_Proccessors_Parallel_2Core() throws IOException {
 		Schedule solution = new ParallelBranchAndBound(2).schedule(new TaskGraph("src/test/resources/Nodes_11_OutTree.dot", 2));
 		assertEquals(350, solution.getTotalTime());
 
 	} 
 	
 	@Test
-	public void test_Nodes_10_2_Proccessors_Parallel() throws IOException {
+	public void test_Nodes_10_2_Proccessors_Parallel_2Core() throws IOException {
 		Schedule solution = new ParallelBranchAndBound(2).schedule(new TaskGraph("src/test/resources/Nodes_10_Random.dot", 2));
 		assertEquals(50, solution.getTotalTime());
 	} 
 	
 	@Test
-	public void test_Nodes_9_2_Proccessors_Parallel() throws IOException {
+	public void test_Nodes_9_2_Proccessors_Parallel_2Core() throws IOException {
 		Schedule solution = new ParallelBranchAndBound(2).schedule(new TaskGraph("src/test/resources/Nodes_9_SeriesParallel.dot", 2));
 		assertEquals(55, solution.getTotalTime());
 	} 
 	
 	@Test
-	public void test_Nodes_8_2_Proccessors_Parallel() throws IOException {
+	public void test_Nodes_8_2_Proccessors_Parallel_2Core() throws IOException {
 		Schedule solution = new ParallelBranchAndBound(2).schedule(new TaskGraph("src/test/resources/Nodes_8_Random.dot", 2));
 		assertEquals(581, solution.getTotalTime());
 
 	} 
 	
 	@Test
-	public void test_Nodes_7_2_Proccessors_Parallel() throws IOException {
+	public void test_Nodes_7_2_Proccessors_Parallel_2Core() throws IOException {
 		Schedule solution = new ParallelBranchAndBound(2).schedule(new TaskGraph("src/test/resources/Nodes_7_OutTree.dot", 2));
 		assertEquals(28, solution.getTotalTime());
 	} 
 	
 	@Test
-	public void test_9middle_ForkandJoin_2_Proccessors_Parallel() throws IOException {
+	public void test_forkjoin9_2_Proccessors_Parallel_2Core() throws IOException {
 		Schedule solution = new ParallelBranchAndBound(2).schedule(new TaskGraph("src/test/resources/forkjoin_9middle.dot", 2));
 		assertEquals(23, solution.getTotalTime());
 	} 
@@ -156,38 +162,120 @@ public class GraphTest {
 	//===================================================================================
 	//===================================================================================
 	//====================				  4 Processors 		     ========================
-	//===================================================================================
+	//=================================2 Parallel Non Visual=============================
 	//===================================================================================
 	
 	@Test
-	public void test_Nodes_10_4_Proccessors_Parallel() throws IOException {
-		Schedule solution = new ParallelBranchAndBound(2).schedule(new TaskGraph("src/test/resources/Nodes_10_Random.dot", 4));
-		assertEquals(50, solution.getTotalTime());
-	} 
-	
-	@Test
-	public void test_Nodes_11_4_Proccessors_Parallel() throws IOException {
+	public void test_Nodes_11_4_Proccessors_Parallel_2Core() throws IOException {
 		Schedule solution = new ParallelBranchAndBound(2).schedule(new TaskGraph("src/test/resources/Nodes_11_OutTree.dot", 4));
 		assertEquals(227, solution.getTotalTime());
 	}
 	
 	@Test
-	public void test_Nodes_9_4_Proccessors_Parallel() throws IOException {
+	public void test_Nodes_10_4_Proccessors_Parallel_2Core() throws IOException {
+		Schedule solution = new ParallelBranchAndBound(2).schedule(new TaskGraph("src/test/resources/Nodes_10_Random.dot", 4));
+		assertEquals(50, solution.getTotalTime());
+	} 
+	
+	@Test
+	public void test_Nodes_9_4_Proccessors_Parallel_2Core() throws IOException {
 		Schedule solution = new ParallelBranchAndBound(2).schedule(new TaskGraph("src/test/resources/Nodes_9_SeriesParallel.dot", 4));
 		assertEquals(55, solution.getTotalTime());
 	} 
 	
 	@Test
-	public void test_Nodes_8_4_Proccessors_Parallel() throws IOException {
+	public void test_Nodes_8_4_Proccessors_Parallel_2Core() throws IOException {
 		Schedule solution = new ParallelBranchAndBound(2).schedule(new TaskGraph("src/test/resources/Nodes_8_Random.dot", 4));
 		assertEquals(581, solution.getTotalTime());
 	}
 	
 	@Test
-	public void test_Nodes_7_4_Proccessors_Parallel() throws IOException {
+	public void test_Nodes_7_4_Proccessors_Parallel_2Core() throws IOException {
 		Schedule solution = new ParallelBranchAndBound(2).schedule(new TaskGraph("src/test/resources/Nodes_7_OutTree.dot", 4));
 		assertEquals(22, solution.getTotalTime());
 	} 
+	
 
+	//===================================================================================
+	//===================================================================================
+	//====================				  2 Processors 		     ========================
+	//=================================4 Parallel Non Visual=============================
+	//===================================================================================
+	
+	
+	@Test
+	public void test_Nodes_11_2_Proccessors_Parallel_4Core() throws IOException {
+		Schedule solution = new ParallelBranchAndBound(4).schedule(new TaskGraph("src/test/resources/Nodes_11_OutTree.dot", 2));
+		assertEquals(350, solution.getTotalTime());
+
+	} 
+	
+	@Test
+	public void test_Nodes_10_2_Proccessors_Parallel_4Core() throws IOException {
+		Schedule solution = new ParallelBranchAndBound(4).schedule(new TaskGraph("src/test/resources/Nodes_10_Random.dot", 2));
+		assertEquals(50, solution.getTotalTime());
+	} 
+	
+	@Test
+	public void test_Nodes_9_2_Proccessors_Parallel_4Core() throws IOException {
+		Schedule solution = new ParallelBranchAndBound(4).schedule(new TaskGraph("src/test/resources/Nodes_9_SeriesParallel.dot", 2));
+		assertEquals(55, solution.getTotalTime());
+	} 
+	
+	@Test
+	public void test_Nodes_8_2_Proccessors_Parallel_4Core() throws IOException {
+		Schedule solution = new ParallelBranchAndBound(4).schedule(new TaskGraph("src/test/resources/Nodes_8_Random.dot", 2));
+		assertEquals(581, solution.getTotalTime());
+
+	} 
+	
+	@Test
+	public void test_Nodes_7_2_Proccessors_Parallel_4Core() throws IOException {
+		Schedule solution = new ParallelBranchAndBound(4).schedule(new TaskGraph("src/test/resources/Nodes_7_OutTree.dot", 2));
+		assertEquals(28, solution.getTotalTime());
+	} 
+	
+	@Test
+	public void test_forkjoin9_2_Proccessors_Parallel_4Core() throws IOException {
+		Schedule solution = new ParallelBranchAndBound(4).schedule(new TaskGraph("src/test/resources/forkjoin_9middle.dot", 2));
+		assertEquals(23, solution.getTotalTime());
+	} 
+	
+	//===================================================================================
+	//===================================================================================
+	//====================				  4 Processors 		     ========================
+	//=================================4 Parallel Non Visual=============================
+	//===================================================================================
+	
+	@Test
+	public void test_Nodes_11_4_Proccessors_Parallel_4Core() throws IOException {
+		Schedule solution = new ParallelBranchAndBound(4).schedule(new TaskGraph("src/test/resources/Nodes_11_OutTree.dot", 4));
+		assertEquals(227, solution.getTotalTime());
+	}
+	
+	@Test
+	public void test_Nodes_10_4_Proccessors_Parallel_4Core() throws IOException {
+		Schedule solution = new ParallelBranchAndBound(4).schedule(new TaskGraph("src/test/resources/Nodes_10_Random.dot", 4));
+		assertEquals(50, solution.getTotalTime());
+	} 
+	
+	@Test
+	public void test_Nodes_9_4_Proccessors_Parallel_4Core() throws IOException {
+		Schedule solution = new ParallelBranchAndBound(4).schedule(new TaskGraph("src/test/resources/Nodes_9_SeriesParallel.dot", 4));
+		assertEquals(55, solution.getTotalTime());
+	} 
+	
+	@Test
+	public void test_Nodes_8_4_Proccessors_Parallel_4Core() throws IOException {
+		Schedule solution = new ParallelBranchAndBound(4).schedule(new TaskGraph("src/test/resources/Nodes_8_Random.dot", 4));
+		assertEquals(581, solution.getTotalTime());
+	}
+	
+	@Test
+	public void test_Nodes_7_4_Proccessors_Parallel_4Core() throws IOException {
+		Schedule solution = new ParallelBranchAndBound(4).schedule(new TaskGraph("src/test/resources/Nodes_7_OutTree.dot", 4));
+		assertEquals(22, solution.getTotalTime());
+	} 
+	
 	
 }
