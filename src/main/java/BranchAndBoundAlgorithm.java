fb26ff673ab7a3a4ec092f512369c6046286ee98
Jordanwmk <jwon223@aucklanduni.ac.nz>
2016-08-25 09:27:19 +1200

    removing print statements

--- src/main/java/BranchAndBoundAlgorithm.java
+++ src/main/java/BranchAndBoundAlgorithm.java
@@ -53,12 +53,12 @@ public class BranchAndBoundAlgorithm implements Algorithm {
 		stack.push(Schedule.getEmptySchedule(taskGraph));
 		scheduleWeAreCurrentlyAt = null;
 		while (!stack.isEmpty()) {
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+//			try {
+//				Thread.sleep(100);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
 			scheduleWeAreCurrentlyAt = stack.pop();
 			List<Schedule> childNodes = scheduleWeAreCurrentlyAt.generateChildren();
 
